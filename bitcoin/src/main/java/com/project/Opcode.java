811fdf0c29af4e50ffbd6c54879cc7f1470b49cb
MaFls-298 <marialefloress@outlook.com>
2026-03-17 19:06:56 -0600

    enum opcodes

--- bitcoin/src/main/java/com/project/Opcode.java
+++ bitcoin/src/main/java/com/project/Opcode.java
@@ -21,14 +21,33 @@ public enum Opcode {
 
     OP_ADD,
     OP_SUB,
-    OP_VERIFY,
+    OP_NUMEQUALVERIFY,
+    OP_LESSTHAN,
+    OP_GREATERTHAN,
+    OP_LESSTHANOREQUAL,
+    OP_GREATERTHANOREQUAL,
 
     OP_DUP,
     OP_DROP,
+    OP_SWAP,
+    OP_OVER,
+
     OP_EQUAL,
     OP_EQUALVERIFY,
+    OP_NOT,
+    OP_BOOLAND,
+    OP_BOOLOR,
 
+    OP_IF,
+    OP_NOTIF,
+    OP_ELSE,
+    OP_ENDIF,
+    OP_VERIFY,
+    OP_RETURN,
+
+    OP_SHA256,
     OP_HASH160,
+    OP_HASH256,
     OP_CHECKSIG,
     OP_CHECKSIGVERIFY,
 
